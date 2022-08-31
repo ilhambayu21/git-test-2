dc6d0a6e7341a3bb07b2141f75cc024c557bae01
ilhambayu21 <bayuilham21@gmail.com>
2022-08-31 20:13:03 +0700

    menambahkan Paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur a ad laudantium assumenda, saepe deleniti aliquam ex nulla eum maiores, voluptatem neque, impedit suscipit placeat nemo facilis perferendis exercitationem minima.</p>
 </body>
 </html>
